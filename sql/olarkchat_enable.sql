5dd3b9cc06e6ef621beb6add92314ddb102e5cb3
Edsel <edsel.lopez@jmaconsulting.biz>
2014-01-21 18:18:17 +0530

    BRES-94 Added secret code validation for Olark

--- sql/olarkchat_enable.sql
+++ sql/olarkchat_enable.sql
@@ -5,7 +5,7 @@ WHERE  cog.name = 'activity_type' AND cov.name IN ('Chat Activity');
 UPDATE civicrm_option_value cov
 INNER JOIN civicrm_option_group cog ON cog.id = cov.option_group_id
 SET cov.is_active = 1
-WHERE cog.name = 'mandrill_secret' AND cov.name  = 'Secret Code';
+WHERE cog.name = 'olark_secret' AND cov.name  = 'Secret Code';
 
 UPDATE civicrm_option_group
 SET is_active = 1
